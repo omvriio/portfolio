d24b0b617c2df87d3e987ff13a66de3a4e956765
omvriio <unimarghadi@gmail.com>
2025-11-17 00:41:37 +0100

    fix: switch to esbuild minifier for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -18,13 +18,7 @@ export default defineConfig({
   },
   build: {
     target: 'es2015',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
